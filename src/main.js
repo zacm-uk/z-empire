87e3e0069be56e400adc784fa2b849c8b43926f5
Zac McChesney <imzacm@gmail.com>
2020-04-07 10:19:31 +0100

    Read nodelist into array

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ const { Node } = require('./Node')
 
 let opts = {}
 
-for (const arg of process.argv) {
+const configArray = process.env.EMPIRE_CONFIG && process.env.EMPIRE_CONFIG.split(' ')
+
+for (const arg of (configArray || process.argv)) {
   let [ key, value ] = arg.split('=')
   if (!key || !value) {
     continue
@@ -15,6 +17,9 @@ for (const arg of process.argv) {
   }
   if (value === 'true' || value === 'false') {
     value = value === 'true'
+  }
+  if (key === 'nodeList') {
+    value = value.split(',')
   }
   if (/^[0-9]*$/gm.test(value)) {
     value = Number(value)
